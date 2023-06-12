11e496af6d5e88cbeac3ab77b3526aaba50f4def
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-06-12 10:52:40 +0600

    19 commit payment system emplement

--- src/pages/dashboard/payment/Payment.jsx
+++ src/pages/dashboard/payment/Payment.jsx
@@ -1,10 +1,20 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../shared/sectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import useCart from "../../../hokes/useCart";
 
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    const price = parseFloat(total.toFixed(2))
     return (
         <div className="w-full">
             <SectionTitle heading='Payment'></SectionTitle>
-            <h3>payment</h3>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm price={price} cart={cart}></CheckoutForm>
+            </Elements>
         </div>
     );
 };
